7bd39e510c949d6020c380aa3dd28263e75724cb
muahahaha55 <phuongnguyenvietbaron123456@gmail.com>
2026-01-13 11:31:29 +0700

    Initial commit with Git LFS

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+path = r"D:\INT3405\SVM_Image_Classification\archive.zip"
+print("Dataset:", path)
