2293d61c3490e85c0cbba4297a98c142bf8cdd74
Nguyen Viet Phuong <phuongnguyenvietbaron123456@gmail.com>
2026-01-13 13:11:31 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import os
-path = r"D:\INT3405\SVM_Image_Classification\archive.zip"
-print("Dataset:", path)
